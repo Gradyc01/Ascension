7b2c5d79fac220c7a7f83be6b992d42097e2937f
AyeDu <71109431+AyeDu-1@users.noreply.github.com>
2024-12-20 13:16:55 +0700

    Added Ascension Key

--- src/main/java/me/depickcator/ascension/Items/Craftable/Crafts.java
+++ src/main/java/me/depickcator/ascension/Items/Craftable/Crafts.java
@@ -1,13 +1,13 @@
-package me.depickcator.ascension.Items.Craftable;
+// package me.depickcator.ascension.Items.Craftable;
 
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.Recipe;
+// import org.bukkit.inventory.ItemStack;
+// import org.bukkit.inventory.Recipe;
 
-public interface Crafts {
-    void recipe();
-    String getKey();
-    ItemStack getResult();
-    Recipe getRecipe();
-    String getDisplayName();
-    int getCraftCost();
-}
+// public interface Crafts {
+//     void recipe();
+//     String getKey();
+//     ItemStack getResult();
+//     Recipe getRecipe();
+//     String getDisplayName();
+//     int getCraftCost();
+// }
---
+++ src/main/java/me/depickcator/ascension/Items/Craftable/Unlocks/AscensionKey.java
@@ -0,0 +1,51 @@
+package me.depickcator.ascension.Items.Craftable.Unlocks;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import me.depickcator.ascension.Ascension;
+import me.depickcator.ascension.General.TextUtil;
+import me.depickcator.ascension.Items.Craftable.Craft;
+import me.depickcator.ascension.Items.UnlockUtil;
+
+public class AscensionKey extends Craft {
+    private static AscensionKey instance;
+    private AscensionKey() {
+        super(1, 999, "Ascension Key", "ascension_key");
+    }
+
+    @Override
+    protected Recipe initRecipe() {
+        NamespacedKey key = new NamespacedKey(plugin, KEY);
+        ShapedRecipe recipe = new ShapedRecipe(key, result);
+        recipe.shape("AAA", "ACB", "BBB");
+        recipe.setIngredient('A', Material.EMERALD);
+        recipe.setIngredient('B', Material.DIAMOND);
+        recipe.setIngredient('C', Material.NETHER_STAR);
+        UnlockUtil.addUnlock(plugin, recipe, MAX_CRAFTS, DISPLAY_NAME);
+        return recipe;
+    }
+
+    @Override
+    protected ItemStack initResult() {
+        ItemStack item = new ItemStack(Material.TRIAL_KEY);
+        ItemMeta meta = item.getItemMeta();
+        meta.setEnchantmentGlintOverride(true);
+        meta.setCustomModelData(Ascension.getInstance().generateModelNumber());
+        meta.displayName(TextUtil.makeText(getDisplayName()));
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    public static AscensionKey getInstance() {
+        if (instance == null) instance = new AscensionKey();
+        return instance;
+    }
+
+
+
+}
